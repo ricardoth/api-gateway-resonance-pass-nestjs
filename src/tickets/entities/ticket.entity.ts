46fe8dfaed49620aea9c1d7690e9fd59c03d0821
ricardoth <ricardotilleriaochoa@gmail.com>
2025-06-30 12:43:47 -0400

    Endpoint Tickets con Paginación

--- src/tickets/entities/ticket.entity.ts
+++ src/tickets/entities/ticket.entity.ts
@@ -1,1 +1,11 @@
-export class Ticket {}
+export class Ticket {
+    idTicket: number;
+    idUsuario: number;
+    idEvento: number;
+    idSector: number;
+    idMedioPago: number;
+    montoPago: number;
+    montoTotal: number;
+    fechaTicket: string;
+    activo: boolean;
+}
